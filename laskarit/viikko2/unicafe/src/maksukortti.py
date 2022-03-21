f6584d1e419b0c04bd2d175a011a568248ccd572
Aleksi Kumpuniemi <aleksi.kumpuniemi@helsinki.fi>
2022-03-21 23:13:49 +0200

    viikko 2, tehtävä 7

--- laskarit/viikko2/unicafe/src/maksukortti.py
+++ laskarit/viikko2/unicafe/src/maksukortti.py
@@ -1,3 +1,6 @@
+from distutils.archive_util import make_archive
+
+
 class Maksukortti:
     def __init__(self, saldo):
         self.saldo = saldo
@@ -16,3 +19,8 @@ class Maksukortti:
         saldo_euroissa = round(self.saldo / 100, 2)
 
         return f"saldo: {saldo_euroissa}"
+
+
+
+kortti = Maksukortti(10)
+print(kortti)
--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,28 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+
+    def test_saldo_oikein_lataamisen_jalkeen(self):
+        self.maksukortti.lataa_rahaa(100)
+        self.assertEqual(str(self.maksukortti), "saldo: 1.1")
+
+    def test_otto_toimii_saldo_vahenee_oikein_kun_saldoa_riittaa(self):
+        self.maksukortti.lataa_rahaa(100)
+        self.maksukortti.ota_rahaa(10)
+        self.assertEqual(str(self.maksukortti), "saldo: 1.0")
+
+    def test_otto_ei_muuta_saldoa_jos_saldoa_liian_vahan(self):
+        self.maksukortti.ota_rahaa(20)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+
+    def test_otto_boolean_toimii_kun_ota_rahaa_saldo_ei_riita(self):
+        arvo = self.maksukortti.ota_rahaa(20)
+        self.assertEqual(arvo, False)
+
+    def test_otto_boolean_toimii_kun_ota_rahaa_saldo_riittaa(self):
+        self.maksukortti.lataa_rahaa(100)
+        arvo = self.maksukortti.ota_rahaa(20)
+        self.assertEqual(arvo, True)
